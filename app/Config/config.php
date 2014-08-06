e7ee683c9c704181f1f252fc4e968380932d260d
tdoornenbal <tdoornenbal@lim-group.com>
2014-08-06 09:13:48 +0200

    better ?

--- app/Config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-Configure::write('Config', array(
-	'paths' => array(
-		'mediaPath' => '/Users/td/Sites/la-roque/app/webroot/uploads/',
-	),
-	'defaultLanguage' => 'fra',
-	'languages' => array('fra' => 'Français', 'nld' => 'Nederlands'),
-));
