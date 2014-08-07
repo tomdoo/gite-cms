c62569c8de3a10fa35c3b17f9394981c0fb3a3a5
tdoornenbal <tdoornenbal@lim-group.com>
2014-08-07 19:24:55 +0200

    install continue with import.sql

--- app/Controller/InstallController.php
+++ app/Controller/InstallController.php
@@ -3,123 +3,130 @@ class InstallController extends AppController {
 	public $layout = 'install';
 	public $uses = array('Post', 'Contact', 'Booking', 'Config', 'Toto');
 
-	public function index() {
-		$steps = array();
+	public function index($install, $sql = null) {
+		if ($sql == 'sql') {
+			if (Configure::check('Config.defaultLanguage')) {
+				$this->set('defaultLanguage', Configure::read('Config.defaultLanguage'));
+			}
+			$this->set('showSql', true);
+		} else {
+			$steps = array();
 
-		// create config file
-		$steps[] = array(
-			'title' => 'Create config file',
-			'text' => '<p>Copy <em>/app/Config/config.php.default</em> to <em>/app/Config/config.php</em></p>',
-			'status' => file_exists(ROOT.DS.APP_DIR.DS.'Config'.DS.'config.php') ? 'success' : 'danger',
-		);
-		
-		// check config
-		$steps[] = array(
-			'title' => 'Complete config file',
-			'text' => '<p>The minimal config file must contains following params (have a look to config.php.default) :</p>
-				<ul>
-					<li>install</li>
-					<li>paths</li>
-					<li>defaultLanguage</li>
-					<li>languages</li>
-					<li>homepages</li>
-				</ul>',
-			'status' => (
-				Configure::check('Config.install')
-				&& Configure::check('Config.paths')
-				&& Configure::check('Config.defaultLanguage')
-				&& Configure::check('Config.languages')
-				&& Configure::check('Config.homepages')) ? 'success' : 'danger',
-		);
-		
-		// create database file
-		$steps[] = array(
-			'title' => 'Create database file',
-			'text' => '<p>Copy <em>/app/Config/database.php.default</em> to <em>/app/Config/database.php</em></p>',
-			'status' => file_exists(ROOT.DS.APP_DIR.DS.'Config'.DS.'database.php') ? 'success' : 'danger',
-		);
+			// create config file
+			$steps[] = array(
+				'title' => 'Create config file',
+				'text' => '<p>Copy <em>/app/Config/config.php.default</em> to <em>/app/Config/config.php</em></p>',
+				'status' => file_exists(ROOT.DS.APP_DIR.DS.'Config'.DS.'config.php') ? 'success' : 'danger',
+			);
+			
+			// check config
+			$steps[] = array(
+				'title' => 'Complete config file',
+				'text' => '<p>The minimal config file must contains following params (have a look to config.php.default) :</p>
+					<ul>
+						<li>install</li>
+						<li>paths</li>
+						<li>defaultLanguage</li>
+						<li>languages</li>
+						<li>homepages</li>
+					</ul>',
+				'status' => (
+					Configure::check('Config.install')
+					&& Configure::check('Config.paths')
+					&& Configure::check('Config.defaultLanguage')
+					&& Configure::check('Config.languages')
+					&& Configure::check('Config.homepages')) ? 'success' : 'danger',
+			);
+			
+			// create database file
+			$steps[] = array(
+				'title' => 'Create database file',
+				'text' => '<p>Copy <em>/app/Config/database.php.default</em> to <em>/app/Config/database.php</em></p>',
+				'status' => file_exists(ROOT.DS.APP_DIR.DS.'Config'.DS.'database.php') ? 'success' : 'danger',
+			);
 
-		// connect database file
-		$message = null;
-		App::uses('ConnectionManager', 'Model');
-		try {
-			$cm = ConnectionManager::getDataSource('default');
-		} catch (CakeException $e) {
-			$message = $e->getMessage();
-		}
-		$steps[] = array(
-			'title' => 'Connect to database',
-			'text' => '<p>'.$message.'</p>',
-			'status' => empty($message) ? 'success' : 'danger',
-		);
+			// connect database file
+			$message = null;
+			App::uses('ConnectionManager', 'Model');
+			try {
+				$cm = ConnectionManager::getDataSource('default');
+			} catch (CakeException $e) {
+				$message = $e->getMessage();
+			}
+			$steps[] = array(
+				'title' => 'Connect to database',
+				'text' => '<p>'.$message.'</p>',
+				'status' => empty($message) ? 'success' : 'danger',
+			);
 
-		// default data check
-		$errors = array();
-		try {
-			$this->Booking->create();
-			foreach (array('id', 'from', 'to', 'full', 'details', 'created', 'modified') as $field) {
-				if (!in_array($field, array_keys($this->Booking->schema()))) {
-					throw new CakeException('Missing field Booking.'.$field);
+			// default data check
+			$errors = array();
+			try {
+				$this->Booking->create();
+				foreach (array('id', 'from', 'to', 'full', 'details', 'created', 'modified') as $field) {
+					if (!in_array($field, array_keys($this->Booking->schema()))) {
+						throw new CakeException('Missing field Booking.'.$field);
+					}
 				}
+			} catch (CakeException $e) {
+				$errors[] = $e->getMessage();
 			}
-		} catch (CakeException $e) {
-			$errors[] = $e->getMessage();
-		}
-		try {
-			$this->Config->create();
-			foreach (array('id', 'name', 'value') as $field) {
-				if (!in_array($field, array_keys($this->Config->schema()))) {
-					throw new CakeException('Missing field Config.'.$field);
+			try {
+				$this->Config->create();
+				foreach (array('id', 'name', 'value') as $field) {
+					if (!in_array($field, array_keys($this->Config->schema()))) {
+						throw new CakeException('Missing field Config.'.$field);
+					}
 				}
-			}
-			if (!$config = $this->Config->find('first', array('conditions' => array('Config.name' => 'title')))) {
-				throw new CakeException('The Config "title" is missing');
-			}
-			if (!$config = $this->Config->find('first', array('conditions' => array('Config.name' => 'baseline')))) {
-				throw new CakeException('The Config "baseline" is missing');
-			}
-		} catch (CakeException $e) {
-			$errors[] = $e->getMessage();
-		}
-		try {
-			$this->Contact->create();
-			foreach (array('id', 'type', 'name', 'email', 'subject', 'text', 'created', 'modified') as $field) {
-				if (!in_array($field, array_keys($this->Contact->schema()))) {
-					throw new CakeException('Missing field Contact.'.$field);
+				if (!$config = $this->Config->find('first', array('conditions' => array('Config.name' => 'title')))) {
+					throw new CakeException('The Config "title" is missing');
 				}
-			}
-		} catch (CakeException $e) {
-			$errors[] = $e->getMessage();
-		}
-		try {
-			$this->Post->create();
-			foreach (array('id', 'type', 'title', 'slug', 'content', 'order', 'online', 'parent_id', 'lft', 'rght', 'created', 'modified') as $field) {
-				if (!in_array($field, array_keys($this->Post->schema()))) {
-					throw new CakeException('Missing field Post.'.$field);
+				if (!$config = $this->Config->find('first', array('conditions' => array('Config.name' => 'baseline')))) {
+					throw new CakeException('The Config "baseline" is missing');
 				}
+			} catch (CakeException $e) {
+				$errors[] = $e->getMessage();
 			}
-			if (!$post = $this->Post->find('first', array('conditions' => array('Post.id' => 1)))) {
-				throw new CakeException('The first post was not found, this one is used as the parent off all the posts');
+			try {
+				$this->Contact->create();
+				foreach (array('id', 'type', 'name', 'email', 'subject', 'text', 'created', 'modified') as $field) {
+					if (!in_array($field, array_keys($this->Contact->schema()))) {
+						throw new CakeException('Missing field Contact.'.$field);
+					}
+				}
+			} catch (CakeException $e) {
+				$errors[] = $e->getMessage();
 			}
-		} catch (CakeException $e) {
-			$errors[] = $e->getMessage();
+			try {
+				$this->Post->create();
+				foreach (array('id', 'type', 'title', 'slug', 'content', 'order', 'online', 'parent_id', 'lft', 'rght', 'created', 'modifgied') as $field) {
+					if (!in_array($field, array_keys($this->Post->schema()))) {
+						throw new CakeException('Missing field Post.'.$field);
+					}
+				}
+				if (!$post = $this->Post->find('first', array('conditions' => array('Post.id' => 1)))) {
+					throw new CakeException('The first post was not found, this one is used as the parent off all the posts');
+				}
+			} catch (CakeException $e) {
+				$errors[] = $e->getMessage();
+			}
+			$steps[] = array(
+				'title' => 'Database check',
+				'text' => 'See the initial import request : <a href="/install/sql" target="blank">go !</a><ul><li>'.implode('</li><li>', $errors).'</li></ul>',
+				'status' => empty($errors) ? 'success' : 'danger',
+			);
+			
+			// end of installation process
+			$success = 0;
+			$total = 0;
+			foreach ($steps as $step) {
+				if ($step['status'] == 'success') {
+					$success++;
+				}
+				$total++;
+			}
+			$this->set('steps', $steps);
+			$this->set('progression', CakeNumber::precision($success*100/$total, 2));
 		}
-		$steps[] = array(
-			'title' => 'Database check',
-			'text' => '<ul><li>'.implode('</li><li>', $errors).'</li></ul>',
-			'status' => empty($errors) ? 'success' : 'danger',
-		);
-		
-		// end of installation process
-		$success = 0;
-		$total = 0;
-		foreach ($steps as $step) {
-			if ($step['status'] == 'success') {
-				$success++;
-			}
-			$total++;
-		}
-		$this->set('steps', $steps);
-		$this->set('progression', CakeNumber::precision($success*100/$total, 2));
 	}
 }
--- app/View/Install/index.ctp
+++ app/View/Install/index.ctp
@@ -1,29 +1,117 @@
-<div class="page-header">
-	<h2>Installation</h2>
-</div>
+<?php if (empty($showSql)) : ?>
+	<div class="page-header">
+		<h2>Installation</h2>
+	</div>
 
-<div class="progress">
-  <div class="progress-bar <?php echo ($progression<100)?'':'progress-bar-success'; ?>" role="progressbar" aria-valuenow="<?php echo $progression; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $progression; ?>%;">
-    <?php echo $progression; ?>%
-  </div>
-</div>
+	<div class="progress">
+	  <div class="progress-bar <?php echo ($progression<100)?'':'progress-bar-success'; ?>" role="progressbar" aria-valuenow="<?php echo $progression; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $progression; ?>%;">
+	    <?php echo $progression; ?>%
+	  </div>
+	</div>
 
-<?php if ($progression < 100) : ?>
-	<?php if (!empty($steps)) : ?>
-		<?php foreach ($steps as $step) : ?>
-			<div class="panel panel-<?php echo $step['status']; ?>">
-				<div class="panel-heading"><?php echo $step['title']; ?></div>
-				<?php if ($step['status'] != 'success') : ?>
-					<div class="panel-body"><?php echo $step['text']; ?></div>
-				<?php endif; ?>
+	<?php if ($progression < 100) : ?>
+		<?php if (!empty($steps)) : ?>
+			<?php foreach ($steps as $step) : ?>
+				<div class="panel panel-<?php echo $step['status']; ?>">
+					<div class="panel-heading"><?php echo $step['title']; ?></div>
+					<?php if ($step['status'] != 'success') : ?>
+						<div class="panel-body"><?php echo $step['text']; ?></div>
+					<?php endif; ?>
+				</div>
+			<?php endforeach; ?>
+		<?php endif; ?>
+	<?php else : ?>
+		<div class="panel panel-success">
+			<div class="panel-heading">Installation completed</div>
+			<div class="panel-body">
+				<p>Now you can set the param "install" at false in the config.php file !</p>
 			</div>
-		<?php endforeach; ?>
+		</div>
 	<?php endif; ?>
 <?php else : ?>
-	<div class="panel panel-success">
-		<div class="panel-heading">Installation completed</div>
-		<div class="panel-body">
-			<p>Now you can set the param "install" at false in the config.php file !</p>
-		</div>
-	</div>
+	<?php if (!empty($defaultLanguage)) : ?>
+	  <pre>
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS `bookings` (
+`id` int(11) NOT NULL,
+  `from` date NOT NULL,
+  `to` date NOT NULL,
+  `full` tinyint(1) NOT NULL,
+  `details` text,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `configs` (
+`id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `value` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+INSERT INTO `configs` (`id`, `name`, `value`) VALUES
+(1, 'title', 'Site title'),
+(2, 'baseline', 'Site baseline');
+CREATE TABLE IF NOT EXISTS `contacts` (
+`id` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL DEFAULT 'contact',
+  `name` varchar(150) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `text` text NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS `i18n` (
+`id` int(10) NOT NULL,
+  `locale` varchar(6) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `foreign_key` int(10) NOT NULL,
+  `field` varchar(255) NOT NULL,
+  `content` text
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+INSERT INTO `i18n` (`id`, `locale`, `model`, `foreign_key`, `field`, `content`) VALUES
+(1, '<?php echo $defaultLanguage; ?>', 'Post', 1, 'title', 'Pages'),
+(2, '<?php echo $defaultLanguage; ?>', 'Post', 1, 'slug', 'pages'),
+(3, '<?php echo $defaultLanguage; ?>', 'Post', 1, 'content', ''),
+(16, '<?php echo $defaultLanguage; ?>', 'Config', 1, 'value', 'Aveyron - Gîte de la Roque'),
+(17, '<?php echo $defaultLanguage; ?>', 'Config', 2, 'value', 'Bienvenue sur le site du gîte de la Roque');
+CREATE TABLE IF NOT EXISTS `posts` (
+`id` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `title` varchar(150) DEFAULT NULL,
+  `slug` varchar(150) DEFAULT NULL,
+  `content` text NOT NULL,
+  `order` int(11) DEFAULT NULL,
+  `online` tinyint(1) NOT NULL DEFAULT '0',
+  `parent_id` int(11) DEFAULT NULL,
+  `lft` int(11) NOT NULL,
+  `rght` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+INSERT INTO `posts` (`id`, `type`, `title`, `slug`, `content`, `order`, `online`, `parent_id`, `lft`, `rght`, `created`, `modified`) VALUES
+(1, 'page', 'Pages', 'pages', '<p>Contenu</p>', NULL, 1, 0, 1, 10, '2014-08-06 05:13:15', '0000-00-00 00:00:00');
+ALTER TABLE `bookings`
+ ADD PRIMARY KEY (`id`);
+ALTER TABLE `configs`
+ ADD PRIMARY KEY (`id`);
+ALTER TABLE `contacts`
+ ADD PRIMARY KEY (`id`);
+ALTER TABLE `i18n`
+ ADD PRIMARY KEY (`id`), ADD KEY `locale` (`locale`), ADD KEY `model` (`model`), ADD KEY `row_id` (`foreign_key`), ADD KEY `field` (`field`);
+ALTER TABLE `posts`
+ ADD PRIMARY KEY (`id`);
+ALTER TABLE `bookings`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `configs`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+ALTER TABLE `contacts`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `i18n`
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+ALTER TABLE `posts`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+	</pre>
+	<?php else : ?>
+	  <p class="text-danger text-center">Check config file first.</p>
+	<?php endif; ?>
 <?php endif; ?>
